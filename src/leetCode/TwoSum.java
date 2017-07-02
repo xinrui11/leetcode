30901aabefa8a0b94e3e195cc84dee5deae89f07
xinrui <xinrui123456@vip.qq.com>
2017-07-02 23:35:40 +0800

    add some note

--- src/leetCode/TwoSum.java
+++ src/leetCode/TwoSum.java
@@ -3,6 +3,12 @@ package leetCode;
  * question
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
  */
 
 import java.util.HashMap;
@@ -14,9 +20,9 @@ import java.util.Map;
 public class TwoSum {
     /**
      * 需要记录下自己的思路
-     * @param nums
-     * @param target
-     * @return
+     * @param nums Given array
+     * @param target specific target
+     * @return int[]
      */
     public int[] twoSumMySolution(int[] nums, int target) {
         int[] result = new int[2];//题目假定只有一组满足的数
