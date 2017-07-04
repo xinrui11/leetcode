bd9b2b234d85dca6d6d9c59102505c84944f032e
xinrui11 <xinrui123456@vip.qq.com>
2017-07-04 15:48:02 +0800

    add new solution to ReverseInteger

--- src/ReverseInteger/ReverseInteger.java
+++ src/ReverseInteger/ReverseInteger.java
@@ -40,6 +40,29 @@ public class ReverseInteger {
         } catch (Exception e) {
             return 0;
         }
+    }
 
+    /**
+     * 网站上支持率最高的solution
+     * Only 15 lines.
+     * If overflow exists, the new result will not equal previous one.
+     * No flags needed. No hard code like 0xf7777777 needed.
+     * Sorry for my bad english.
+     * 翻译：只有15行代码，如果出现了溢出，新的结果不会等于之前的结果，不需要状态判断，也不需要硬编码。
+     * 基本思路是原数不断除以10，结果不断乘以10，正负数不需要考虑
+     */
+    public int reverseFast(int x) {
+        int result = 0;
+        while (x != 0){
+            int tail = x % 10;
+            int newResult = tail + result * 10;
+            if(result != (newResult-tail)/10 ){
+                return 0;
+            }
+            //x = (x - tail) / 10;//此计算放在判断后面，可以提高效率，整数相除会自动抹去小数，所以不用减去尾数
+            x = x / 10;
+            result = newResult;
+        }
+        return result;
     }
 }
