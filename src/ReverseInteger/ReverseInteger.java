38af66fc9db49f2837e434df2d6df846902c3f73
xinrui <xinrui11@users.noreply.github.com>
2017-07-04 11:46:57 +0800

    reorganize the directory structure

--- src/ReverseInteger/ReverseInteger.java
+++ src/ReverseInteger/ReverseInteger.java
@@ -27,7 +27,7 @@ public class ReverseInteger {
      */
     public int reverse(int x) {
         String s = String.valueOf(x);
-        String re = new String();
+        String re = new String();///1
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) == '-') {
                 re = "-" + re;
