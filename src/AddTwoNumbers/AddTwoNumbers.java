a7066b2890b2b3daea634081404012864f09b586
xinrui <xinrui123456@vip.qq.com>
2017-07-07 22:07:21 +0800

    add new solutions for AddTwoNumber

--- src/AddTwoNumbers/AddTwoNumbers.java
+++ src/AddTwoNumbers/AddTwoNumbers.java
@@ -44,4 +44,54 @@ public class AddTwoNumbers {
         return p2;
     }
 
+    /**
+     * 官方揭发
+     */
+    public ListNode addTwoNumbersOfficial(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    /**
+     * 最高用户票数
+     */
+    public ListNode addTwoNumbersMostVoted(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode sentinel = new ListNode(0);
+        ListNode d = sentinel;
+        int sum = 0;
+        while (c1 != null || c2 != null) {
+            sum /= 10;
+            if (c1 != null) {
+                sum += c1.val;
+                c1 = c1.next;
+            }
+            if (c2 != null) {
+                sum += c2.val;
+                c2 = c2.next;
+            }
+            d.next = new ListNode(sum % 10);
+            d = d.next;
+        }
+        if (sum / 10 == 1)
+            d.next = new ListNode(1);
+        return sentinel.next;
+    }
+
 }
