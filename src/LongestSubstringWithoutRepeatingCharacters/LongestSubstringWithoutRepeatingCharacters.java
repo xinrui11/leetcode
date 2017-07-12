75c7a2c85d44a1263e932a7b53056c31cfbc2eab
xinrui11 <xinrui123456@vip.qq.com>
2017-07-12 17:36:27 +0800

    add solution

--- src/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,10 @@
 package LongestSubstringWithoutRepeatingCharacters;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Given a string, find the length of the longest substring without repeating characters.
  *
@@ -17,7 +22,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
         String temp = "";
         int start, end = 1;
         for(int i = 0; i < s.length(); i++){
-            start = i;
+            start = i;//start前移，中间的字段肯定不重复
             temp = s.substring(start, end);
             while(end + 1 <= s.length() && !temp.contains(s.substring(end, end + 1))){
                 end++;
@@ -28,5 +33,56 @@ public class LongestSubstringWithoutRepeatingCharacters {
             }
         }
         return answer;
+    }
+
+    /*----------------------以下为官方解法-----------------------*/
+
+    //此方法与我的思路差不多，官方将其称为slide，最多只需要2n步
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    //再次优化后的方法，进一步提升时间复杂度，只需要n步
+    public int lengthOfLongestSubstring3(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
+    //优化空间复杂度，非重复字符最多不过256个
+    //int[26] for Letters 'a' - 'z' or 'A' - 'Z'
+    //int[128] for ASCII
+    //int[256] for Extended ASCII
+    public int lengthOfLongestSubstring4(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
     }
 }
