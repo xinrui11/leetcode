ef296f7350b957bd8ff0247ed3a3fa907fda8967
xinrui11 <xinrui123456@vip.qq.com>
2017-07-18 17:31:39 +0800

    add solution

--- src/LongestPalindromicSubstring/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring/LongestPalindromicSubstring.java
@@ -1,6 +1,5 @@
 package LongestPalindromicSubstring;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -22,27 +21,39 @@ public class LongestPalindromicSubstring {
     /**
      * 最长回文字
      */
+    //最初的解法，暴力寻找，时间复杂度直上n`3，leecode会报超时
     public String longestPalindrome(String s) {
         if(s.length() <= 1){
             return s;
         }
-        String longest = "";
+        String longest = new String();
+        String temp;
         for(int i = 0; i < s.length(); i++){
-            Map<Integer, Character> m = new HashMap<>();
-            int key = 0;
-            m.put(key, s.charAt(i));
-            for(int j = i+1; j < s.length(); j++){
-                key++;
-                m.put(key, s.charAt(j));
-                if(isPalindrome(m)){
-                    if(longest.length() < j + 1 - i){
-                        longest = s.substring(i, j + 1);
+            for(int j = s.length(); j > i; j--){
+                temp = s.substring(i, j);
+                if(isPalindrome(temp)){
+                    if(longest.length() < temp.length()){
+                        longest = temp;
                     }
                 }
             }
         }
         return longest;
     }
+
+    private boolean isPalindrome(String m){
+        if(m.length() <= 1){
+            return true;
+        }
+        int halfSize = m.length() / 2;
+        for(int i = 0;i < halfSize; i++){
+            if(m.charAt(i) != m.charAt(m.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
     private boolean isPalindrome(Map<Integer, Character> m){
         if(m.size() <= 1){
             return true;
@@ -55,4 +66,77 @@ public class LongestPalindromicSubstring {
         }
         return true;
     }
+
+    //中心拓展法，时间复杂度n`2，leetcode通过
+    public String longestPalindrome2(String s) {
+        if(s.length() <= 1){
+            return s;
+        }
+        String longest = new String();
+        for(int i = 0; i < s.length() - 1; i++){
+            String temp = centerExpand(s,i,i+1);
+            if(longest.length()<temp.length())
+                longest = temp;
+            temp = centerExpand(s,i,i);
+            if(longest.length()<temp.length())
+                longest = temp;
+        }
+        return longest;
+    }
+    private String centerExpand(String s, int start, int end){
+        while(start >=0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            start--;
+            end++;
+        }
+        return s.substring(start + 1, end );
+    }
+
+    //将s倒置为s2，s和s2的最长的公共的字符串即为最长回文字符串，时间复杂度也是n`2，leetcode会报超时
+    public String longestPalindrome3(String s) {
+        String s2 = new String();
+        for(int i = s.length() - 1; i >= 0; i--){
+            s2 = s2 + s.charAt(i);
+        }
+        String longest = new String();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String temp = s.substring(i,j);
+                String temp2 = s2.substring(s.length() - j,s.length() - i);
+                if(temp.equals(temp2) && temp.length() > longest.length()){
+                    longest = temp;
+                }
+            }
+        }
+        return longest;
+    }
+
+    //动态规划
+    public String longestPalindrome4(String s) {
+        boolean[][] p = new boolean[1000][1000];
+        int start = 0,maxLength = 0;
+        for (int i = 0; i < 1000; i++) {
+            p[i][i] = true;
+            if(maxLength == 0){
+                start = i;
+                maxLength = 1;
+            }
+            if(i + 1 < s.length() && s.charAt(i) == s.charAt(i+1)){
+                p[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+        for(int len = 3;len <= s.length(); len++ ){
+            for (int i = 0; i < s.length(); i++) {
+                int j = i + len - 1;
+                if(j < s.length() && p[i + 1][j - 1] && s.charAt(i)==s.charAt(j)){
+                    p[i][j] = true;
+                    start = i;
+                    maxLength = len;
+                }
+            }
+        }
+        return s.substring(start, start + maxLength);
+    }
+
 }
--- src/LongestPalindromicSubstring/LongestPalindromicSubstringTest.java
+++ src/LongestPalindromicSubstring/LongestPalindromicSubstringTest.java
@@ -7,9 +7,12 @@ public class LongestPalindromicSubstringTest extends TestCase {
     @Test
     public void testlongestPalindrome() throws Exception {
         LongestPalindromicSubstring l = new LongestPalindromicSubstring();
-        String input = "asdqweewsdfewq";
+        String input = "asdfgfdsaaaa";
         System.out.println("input is:" + input);
-        System.out.println("out is:" + l.longestPalindrome(input));
+        System.out.println("out is:" + l.longestPalindrome4(input));
+        input = "ccc";
+        System.out.println("input is:" + input);
+        System.out.println("out is:" + l.longestPalindrome4(input));
     }
 
 }
