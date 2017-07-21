7b004d414d74ffc4e3a05dd90cff29216794fdd3
xinrui11 <xinrui123456@vip.qq.com>
2017-07-21 21:35:14 +0800

    add solution

--- src/ZigZagConversion/ZigZagConversion.java
+++ src/ZigZagConversion/ZigZagConversion.java
@@ -1,5 +1,8 @@
 package ZigZagConversion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ZigZagConversion {
     /**
      The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
@@ -14,23 +17,33 @@ public class ZigZagConversion {
      convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
      */
     public String convert(String s, int numRows) {
-        int[][] result = new int[numRows][s.length()];
-        int space = numRows * 2 - 3;
+        Map<Integer,String> result = new HashMap<>();
+        boolean flag = true;
         for (int i = 0; i < s.length();) {
-            for (int j = 0; j < numRows; j++,i++) {
-                result[j]
+            for (int j = 0; j < numRows && i < s.length(); i++) {
+                String temp = result.get(j);
+                if(temp != null){
+                    result.put(j,temp+s.charAt(i));
+                }
+                else{
+                    result.put(j,""+s.charAt(i));
+                }
+                if(flag)
+                    j++;
+                else
+                    j--;
+                if(j == numRows - 1){
+                    flag = false;
+                }
+                if(j == 0){
+                    flag = true;
+                }
             }
         }
-        for (int i = 0; i < numRows; i++) {
-            if(i >= numRows / 2){
-                space = space + ((i - (numRows / 2)) * 2);
-            } else {
-                space = space - (i * 2);
-            }
-            for (int j = i; j < s.length(); j=+space) {
-                result = result + s.charAt(j)+ ;
-            }
+        String re = "";
+        for(Map.Entry<Integer, String> entry : result.entrySet()){
+            re = re + entry.getValue();
         }
-        return result;
+        return re;
     }
 }
