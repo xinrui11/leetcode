ebc180e48a84b12b066e27f779bf21ab3b44693e
xinrui11 <xinrui123456@vip.qq.com>
2017-07-31 10:12:29 +0800

    add solution

--- src/RemoveNthNodeFromEndofList/RemoveNthNodeFromEndofList.java
+++ src/RemoveNthNodeFromEndofList/RemoveNthNodeFromEndofList.java
@@ -27,4 +27,44 @@ public class RemoveNthNodeFromEndofList {
         }
         return head;
     }
+
+    /** offcial solution **/
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;//use next to substitute head
+        int length  = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+
+    //In one pass
+    public ListNode removeNthFromEnd3(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        //! ListNode first = head;
+        //! ListNode second = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for(int i = 1; i <= n + 1; i++){
+            first = first.next;
+        }
+        while (first != null){
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
 }
