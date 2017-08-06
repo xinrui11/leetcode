51ef2ddbf363a030eb339cc9260ca590c39d7194
xinrui <xinrui123456@vip.qq.com>
2017-08-06 23:55:46 +0800

    add solution

---
+++ src/DivideTwoIntegers/DivideTwoIntegersTest.java
@@ -0,0 +1,12 @@
+package DivideTwoIntegers;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by xinrui on 2017-8-6.
+ */
+public class DivideTwoIntegersTest extends TestCase {
+    public void testDivide() throws Exception {
+    }
+
+}
