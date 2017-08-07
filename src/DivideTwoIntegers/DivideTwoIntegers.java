72b0a598ed2bf160b240d8ab6c607916f1dec011
xinrui11 <xinrui123456@vip.qq.com>
2017-08-07 15:58:06 +0800

    add solution

--- src/DivideTwoIntegers/DivideTwoIntegers.java
+++ src/DivideTwoIntegers/DivideTwoIntegers.java
@@ -1,6 +1,13 @@
 package DivideTwoIntegers;
 
+/**
+ Divide two integers without using multiplication, division and mod operator.
+
+ If it is overflow, return MAX_INT.
+ */
+
 public class DivideTwoIntegers {
+    //this solution will be TLE
     public int divide(int dividend, int divisor) {
         if(divisor == 0){
             return Integer.MAX_VALUE;
@@ -26,6 +33,49 @@ public class DivideTwoIntegers {
         if(flag){
             res = - res;
         }
+        if(res+(-res) != 0){
+            return Integer.MAX_VALUE;
+        }
         return res;
+    }
+
+    public int divide2(int dividend, int divisor) {
+        if(divisor == 0){
+            return Integer.MAX_VALUE;
+        }
+        boolean flag = false;
+        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)){
+            flag = true;
+        }
+        if(dividend > 0){
+            dividend = - dividend;
+        }
+        if(divisor > 0){
+            divisor = - divisor;
+        }
+        long res = divideRecursive(dividend, divisor);
+        if(flag){
+            res = - res;
+        }
+        if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE){
+            return Integer.MAX_VALUE;
+        }
+        return (int)res;
+    }
+
+    private long divideRecursive(int dividend, int divisor){
+        if(dividend > divisor){
+            return 0;
+        }
+        //  Find the largest multiple so that (divisor * multiple <= dividend),
+        //  whereas we are moving with stride 1, 2, 4, 8, 16...2^n for performance reason.
+        //  Think this as a binary search.
+        long sum = divisor;
+        long count = 1;
+        while(sum+sum >= dividend){
+            sum += sum;
+            count += count;
+        }
+        return count + divideRecursive(dividend - (int)sum, divisor);
     }
 }
--- src/DivideTwoIntegers/DivideTwoIntegersTest.java
+++ src/DivideTwoIntegers/DivideTwoIntegersTest.java
@@ -7,6 +7,11 @@ import junit.framework.TestCase;
  */
 public class DivideTwoIntegersTest extends TestCase {
     public void testDivide() throws Exception {
+        DivideTwoIntegers d = new DivideTwoIntegers();
+        System.out.println(d.divide(15,3));
+        System.out.println(d.divide2(15,3));
+        System.out.println(d.divide2(-2147483648,-1));
+
     }
 
 }
