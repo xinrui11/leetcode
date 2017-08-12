a9c19f30cb9c70476cb4f10ce2ba9e9139c385be
xinrui11 <xinrui123456@vip.qq.com>
2017-08-12 15:25:37 +0800

    add solution

--- src/LongestValidParentheses/LongestValidParentheses.java
+++ src/LongestValidParentheses/LongestValidParentheses.java
@@ -1,5 +1,7 @@
 package LongestValidParentheses;
 
+import java.util.Stack;
+
 /**
  Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
 
@@ -80,41 +82,90 @@ public class LongestValidParentheses {
     }
 
     public int longestValidParenthesesMYDP(String s) {
-        int[] d1 = new int[s.length()];
-        int[] d2 = new int[s.length()];
         int res = 0;
         char[] c = s.toCharArray();
-        for (int i = 0; i < c.length; i++) {
-            if(i == 0){
-                if(c[i] == '('){
-                    d1[i] = 0;
-                    d2[i] = 1;
-                } else {
-                    d1[i] = 0;
-                    d2[i] = 0;
-                }
+        int[] d = new int[c.length];
+        /*for (int i = 1; i < c.length; i++) {
+            if(c[i] == '('){//这里不需要赋值0，int型数组会自动初始化为0
+                d[i] = 0;
             } else {
-                if(c[i] == '('){
-                    if(d2[i - 1] > 0){
-                        d1[i] = 0;
-                    } else {
-                        d1[i] = d1[i - 1];
-                    }
-                    d2[i] = d2[i - 1] + 1;
+                if(c[i - 1] == '('){
+                    d[i] = i - 2 >= 0 ? d[i - 2] + 2: 2;
                 } else {
-                    if(d2[i - 1] > 0){
-                        d1[i] = d1[i - 1] + 2;
-                        d2[i] = d2[i - 1] - 1;
-                        if(res < d1[i]){
-                            res = d1[i];
-                        }
+                    if(i - d[i - 1] - 1 > 0 && c[i - d[i - 1] - 1] == '('){
+                        d[i] = d[i - 1] + 2;
                     } else {
-                        d1[i] = 0;
-                        d2[i] = 0;
+                        d[i] = 0;
                     }
+                }
+                if(res < d[i]){
+                    res = d[i];
+                }
+            }
+        }*/
+        for (int i = 1; i < c.length; i++) {
+            if(c[i] == ')'){
+                if(c[i - 1] == '('){//looks like ...()
+                    d[i] = i - 2 >= 0 ? d[i - 2] + 2: 2;
+                } else if(i - d[i - 1] - 1 >= 0 && c[i - d[i - 1] - 1] == '('){//looks like ...))
+                    //this is the key
+                    d[i] = d[i - 1] + ((i - d[i - 1]) >= 2 ? d[i - d[i - 1] - 2] : 0)  + 2;
+                }
+                if(res < d[i]){
+                    res = d[i];
                 }
             }
         }
         return res;
     }
+
+    public int longestValidParenthesesUseStack(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+
+    public int longestValidParenthesesTwoPass(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right >= left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left >= right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
