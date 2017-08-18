67416ff39832658c6296e6bf73f1e20cd43533f7
xinrui11 <xinrui123456@vip.qq.com>
2017-08-18 17:41:04 +0800

    add solution

--- src/FirstMissingPositive/FirstMissingPositive.java
+++ src/FirstMissingPositive/FirstMissingPositive.java
@@ -12,6 +12,38 @@ package FirstMissingPositive;
 
 public class FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
-
+        if(nums.length == 0){
+            return 1;
+        }
+        int res = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] < 0){
+                int temp = nums[i - 1];
+                nums[i - 1] = nums[i];
+                nums[i] = temp;
+                if(res == i - 1){
+                    res = i;
+                }
+                continue;
+            } else {
+                if(nums[i]<nums[i - 1]){
+                    if(nums[i - 1] - nums[i] == 1){
+                        int temp = nums[i - 1];
+                        nums[i - 1] = nums[i];
+                        nums[i] = temp;
+                        res = i;
+                    } else {
+                        res = i - 1;
+                    }
+                } else {
+                    if(nums[i] - nums[i - 1] == 1){
+                        res = i;
+                    } else {
+                        res = i - 1;
+                    }
+                }
+            }
+        }
+        return nums[res] + 1;
     }
 }
