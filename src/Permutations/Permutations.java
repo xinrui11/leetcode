24ca2b50a40ed39e9059e27708321fd54cdd0a37
xinrui11 <xinrui123456@vip.qq.com>
2017-08-30 10:23:20 +0800

    add solution

--- src/Permutations/Permutations.java
+++ src/Permutations/Permutations.java
@@ -1,6 +1,7 @@
 package Permutations;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -19,6 +20,9 @@ import java.util.List;
  */
 public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return new ArrayList<>();
+        }
         List<List<Integer>> res = new ArrayList<>();
         permute(nums, new ArrayList<>(), res);
         return res;
@@ -27,20 +31,91 @@ public class Permutations {
     private void permute(int[] nums, List<Integer> temp, List<List<Integer>> res){
         if(nums.length - temp.size() == 1){
             for(int num : nums){
-                if(temp.contains(num))
-                    continue;
-                List<Integer> copy = new ArrayList<>(temp);
-                copy.add(num);
-                res.add(copy);
+                if(!temp.contains(num)){
+                    List<Integer> copy = new ArrayList<>(temp);
+                    copy.add(num);
+                    res.add(copy);
+                }
             }
         } else {
             for(int num : nums){
-                if(temp.contains(num))
-                    continue;
-                temp.add(num);
-                List<Integer> copy = new ArrayList<>(temp);
-                permute(nums, copy, res);
+                if(!temp.contains(num)){
+                    temp.add(num);
+                    List<Integer> copy = new ArrayList<>(temp);
+                    permute(nums, copy, res);
+                    temp.remove(temp.size() - 1);
+                }
             }
         }
+    }
+
+    //a solution in discuss,the main idea is like mine
+    public List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        // Arrays.sort(nums); // not necessary
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for(int i = 0; i < nums.length; i++){
+                if(tempList.contains(nums[i])) continue; // element already exists, skip
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    //a AC solution
+
+    /**
+     * the basic idea is, to permute n numbers, we can add the nth number into the resulting List<List<Integer>> from the n-1 numbers, in every possible position.
+
+     For example, if the input num[] is {1,2,3}: First, add 1 into the initial List<List<Integer>> (let's call it "answer").
+
+     Then, 2 can be added in front or after 1. So we have to copy the List<Integer> in answer (it's just {1}), add 2 in position 0 of {1}, then copy the original {1} again, and add 2 in position 1. Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
+
+     Then we have to add 3. first copy {2,1} and {1,2}, add 3 in position 0; then copy {2,1} and {1,2}, and add 3 into position 1, then do the same thing for position 3. Finally we have 2*3=6 lists in answer, which is what we want.
+     */
+    public List<List<Integer>> permute3(int[] num) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if (num.length ==0) return ans;
+        List<Integer> l0 = new ArrayList<Integer>();
+        l0.add(num[0]);
+        ans.add(l0);
+        for (int i = 1; i< num.length; ++i){
+            List<List<Integer>> new_ans = new ArrayList<List<Integer>>();
+            for (int j = 0; j<=i; ++j){
+                for (List<Integer> l : ans){
+                    List<Integer> new_l = new ArrayList<Integer>(l);
+                    new_l.add(j,num[i]);
+                    new_ans.add(new_l);
+                }
+            }
+            ans = new_ans;
+        }
+        return ans;
+    }
+
+    //the main idea is like permute3,but through use LinkedList make solution better
+    public List<List<Integer>> permute4(int[] num) {
+        LinkedList<List<Integer>> res = new LinkedList<List<Integer>>();
+        res.add(new ArrayList<Integer>());
+        for (int n : num) {
+            int size = res.size();
+            for (; size > 0; size--) {
+                List<Integer> r = res.pollFirst();
+                for (int i = 0; i <= r.size(); i++) {
+                    List<Integer> t = new ArrayList<Integer>(r);
+                    t.add(i, n);
+                    res.add(t);
+                }
+            }
+        }
+        return res;
     }
 }
