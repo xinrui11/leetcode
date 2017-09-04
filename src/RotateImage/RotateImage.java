7ba4373cddfdfb1c128cbcfbb7f7acb2bd0d560e
xinrui11 <xinrui123456@vip.qq.com>
2017-09-04 10:46:38 +0800

    add solution

--- src/RotateImage/RotateImage.java
+++ src/RotateImage/RotateImage.java
@@ -80,4 +80,30 @@ public class RotateImage {
         }
         return allcount;
     }
+
+    //Solution in discuss
+    /**
+     [
+         [1,2,3],          [1,4,7],           [7,4,1],
+         [4,5,6],   -->    [2,5,8],    -->    [8,5,2],
+         [7,8,9]           [3,6,9]            [9,6,3]
+     ]
+     */
+    public void rotate2(int[][] matrix) {
+        int size = matrix.length;
+        for(int i = 0; i < size; i++){
+            for(int j = i; j < size; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][size - j - 1];
+                matrix[i][size - j - 1] = temp;
+            }
+        }
+    }
 }
